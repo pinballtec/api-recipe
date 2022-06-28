eaec97fecee0b4c8b5f2b1f266a9b64129544222
Daniil_Boika <daniilboiko99@gmail.com>
2022-06-28 18:23:35 +0200

    added unit test for db

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,29 @@
+# Test for Django management commands
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psy2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTest(SimpleTestCase):
+
+    def test_for_db_ready(self, patched_check):
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_for_db_delay(self, patched_check, patched_sleep):
+        patched_check.side_effect = [Psy2Error] * 2 + [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=['default'])
