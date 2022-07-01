33c1c1441601af1b457db474a2e6d21a56b0cd27
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-01 18:37:47 +0200

    tests for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,19 @@
+import time
 from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as PsycopgError
+from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for db')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(PsycopgError, OperationalError):
+                self.stdout.write('Database is not responding')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database is available'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,13 +17,13 @@ class CommandTest(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_for_db_delay(self, patched_check, patched_sleep):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         patched_check.side_effect = [Psy2Error] * 2 + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
