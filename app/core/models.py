43c19d0ac526b2ddf16368f60e69ef348944d8ce
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-02 17:59:59 +0200

    added custom user model with user manager + tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
 
