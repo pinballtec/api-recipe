5b4828e87abfa75f1442496aadbaee01d8d9ad47
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-02 21:28:37 +0200

    added tests for emailadrs

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager, 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
