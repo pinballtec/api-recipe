27071bc02b736cba465f06e19eeb55ec53fde2e9
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-03 14:38:46 +0200

    added custom admin panel + tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
