407471d15c60243866302eefe04353040ed4d71e
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-11 14:29:07 +0200

    token auth added + tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +12,27 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            requests=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _('Authentication Error')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from .serializers import UserSerializer, AuthTokenSerializer
 # Create your views here.
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES 
