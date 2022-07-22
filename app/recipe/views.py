dc7072a826efb15f939db6d4b20e11fc681c5554
Daniil_Boika <daniilboiko99@gmail.com>
2022-07-22 16:18:00 +0200

    added save view for recipe + tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -24,3 +24,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """creating new recipe"""
+        serializer.save(user=self.request.user)
